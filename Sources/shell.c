46f79f54aee2e1220ed35f3619337aa6ea5a1954
Agustin Sanchez <trorik23@gmail.com>
2017-06-02 20:30:56 -0300

    switch de comandos listos

--- Sources/shell.c
+++ Sources/shell.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+#include "bufferrx.h"
 
 void shell_A(void);
 void shell_B(void);
@@ -15,79 +16,91 @@ void shell_num(void);
 
 // TODO(agus): refactor para usar matriz en la comparacion
 // 	       de comandos
-void shell_execute(char * command){
-	// TODO: agregar lib con funcion is_num
-	/* while(SEA NUMERO) */
-	/* } */
 
-	/* if(ES TRUE){ */
-	/* llamo a shell con num */
-	/* return */
-	/* } */
-
-	// ESTO ES HERMOSO
-	switch (length(command)) {
-		case 1:
-			switch (command[0]) {
-				case 'A':
-					shell_A();
-					break;
-				case 'B':
-					shell_B();
-					break;
-				case 'C':
-					shell_C();
-					break;
-				default:
-					shell_error();
-			}
+void shell_execute(char dim) {
+	char r=0;
+	char num=0;
+	while(r<dim && bufferrx_buff[r]-'0'>=0 && bufferrx_buff[r]-'0'<=9){
+		//es un digito
+		num=num*10;
+		num+=bufferrx_buff[r];
+		r++;		
+	}
+	if(r==dim){
+		//todos digitos
+		shell_num(num);
+		return;
+	}	
+	//si no correspondia a un numero sigue
+	
+	switch (dim) {
+	case 1:
+		switch (bufferrx_buff[0]) {
+		case 'A':
+			shell_A();
 			break;
-		case 2:
-			if(command[0]=='O' && command[1]=='N') shell_on();
+		case 'B':
+			shell_B();
 			break;
-		case 3:
-			if(command[0]=='O' && command[1]=='F' && command[2]=='F') shell_off();
-			break;
-		case 5:
-			if(command[0]=='R' && command[1]=='E' && command[2]=='S' && command[3]=='E' && command[4]=='T') shell_reset();
+		case 'C':
+			shell_C();
 			break;
 		default:
 			shell_error();
+		}
+		break;
+	case 2:
+		if (bufferrx_buff[0] == 'O' && bufferrx_buff[1] == 'N')
+			shell_on();
+		break;
+	case 3:
+		if (bufferrx_buff[0] == 'O' && bufferrx_buff[1] == 'F'
+				&& bufferrx_buff[2] == 'F')
+			shell_off();
+		break;
+	case 5:
+		if (bufferrx_buff[0] == 'R' && bufferrx_buff[1] == 'E'
+				&& bufferrx_buff[2] == 'S' && bufferrx_buff[3] == 'E'
+				&& bufferrx_buff[4] == 'T')
+			shell_reset();
+		break;
+	default:
+		shell_error();
 	}
 }
 
 // TODO: en estos casos, habria que setear el timer TPMC1V no?
-void shell_A(void){
+void shell_A(void) {
 	// PERIODO ==5seg
 	// TPM1C1V=????
 }
 
-void shell_B(void){
+void shell_B(void) {
 	// PERIODO ==10seg
 	// TPM1C1V=????
 }
 
-void shell_C(void){
+void shell_C(void) {
 	// PERIODO ==15seg
 	// TPM1C1V=????
 }
 
-void shell_on(void){
+void shell_on(void) {
 
 }
 
-void shell_off(void){
+void shell_off(void) {
 
 }
 
-void shell_reset(void){
+void shell_reset(void) {
 
 }
 
-void shell_error(void){
+void shell_error(void) {
 
 }
 
-void shell_num(void){
+void shell_num(void) {
 
 }
