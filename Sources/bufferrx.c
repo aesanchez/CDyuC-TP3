2d8bc10ea9b89a6a2ffd49001e5d926b511f2eb8
Agustin Sanchez <trorik23@gmail.com>
2017-06-03 11:32:08 -0300

    control de caracteres en bufferrx
    Ahora todo caracter que no sea alfa-numerico será ignorado.
    En caso de recibir una letra mayuscula, será convertida a minuscula.
    Todo esto para desligar shell de validaciones y/o conversiones
    innecesarias y de no mandarle basura.

--- Sources/bufferrx.c
+++ Sources/bufferrx.c
@@ -13,13 +13,33 @@ void bufferrx_receive(void) {
 	buffertx_send_char(SCID);
 	if (SCID == '\r') {
 		buffertx_send_str("\n > ");
-		if (i > 0) shell_execute(i);
+		if (i > 0)
+			shell_execute(i);
 		i = 0;
 		return;
 	}
-	bufferrx_buff[i++] = SCID;
+	push_filtered(SCID);
 	if (i == LEN) {
 		i = 0;
 		buffertx_send_str("\r\nCOMMAND OUT OF BOUND\r\n > ");
 	}
+}
+void push_filtered(char c) {
+	if (c >= '0' && c <= '9') {
+		//es un digito >> no hacer nada
+		bufferrx_buff[i++] = c;
+		return;
+	}
+	if (c >= 'A' && c <= 'Z') {
+		//upper caso to lower case
+		bufferrx_buff[i++] = c + ('a' - 'A');
+		return;
+	}
+	if (c >= 'a' && c <= 'z') {
+		//lower case >> no hacer nada
+		bufferrx_buff[i++] = c;
+		return;
+	}
+	//en este punto ya no son caracteres validos
+	//por lo que no se lo pushea en el buffer
 }
