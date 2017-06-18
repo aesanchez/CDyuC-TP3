c28ab21c415c7d323130e502986e60faa58cd146
Matias Pan <matias.pan26@gmail.com>
2017-06-18 16:18:57 -0300

    bufferrx comentado

--- Project_Headers/bufferrx.h
+++ Project_Headers/bufferrx.h
@@ -1,9 +1,17 @@
 #ifndef BUFFERRX_H_
 #define BUFFERRX_H_
 
+// FLAG_RECEIVED indica que se leyo un caracter
 extern volatile char FLAG_RECEIVED;
 
+// bufferrx_get_char devuelve el ultimo caracter leido
 char bufferrx_get_char(void);
+
+// bufferrx_receive_interrupt es llamado desde la interrupcion
+// de recepcion, un llamado a esta funcion indica que se ingreso
+// un caracter. Dicho caracter va a estar disponible en el SCID,
+// el cual se almacena en la variable car. Luego mediante el
+// FLAG_RECEIVED se le avisa a los que escuchan que se recibio un caracter
 void bufferrx_receive_interrupt(void);
 
 #endif
--- Sources/bufferrx.c
+++ Sources/bufferrx.c
@@ -4,11 +4,17 @@
 volatile char FLAG_RECEIVED=0;
 char car;
 
+// bufferrx_receive_interrupt es llamado desde la interrupcion
+// de recepcion, un llamado a esta funcion indica que se ingreso
+// un caracter. Dicho caracter va a estar disponible en el SCID,
+// el cual se almacena en la variable car. Luego mediante el
+// FLAG_RECEIVED se le avisa a los que escuchan que se recibio un caracter
 void bufferrx_receive_interrupt(){
 	car=SCID;
 	FLAG_RECEIVED=1;
 }
 
+// bufferrx_get_char devuelve el ultimo caracter leido
 char bufferrx_get_char(void){
 	return car;
 }
